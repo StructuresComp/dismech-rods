51b49d613c0e89e595fa8f5bed9d7cf2f1ab9e0c
Radha Lahoti <75471081+Radha-Lahoti@users.noreply.github.com>
2024-01-26 17:18:09 -0800

    Add comments for doc to robotDescription.h

--- src/robotDescription.h
+++ src/robotDescription.h
@@ -1,3 +1,14 @@
+/**
+ * @file robotDescription.h
+ * @author Andrew Choi
+ * @brief This is the file with all input parameters which the user can change according to the requirement
+ * @version 0.1
+ * @date 2024-01-11
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
+
 #ifndef ROBOTDESCRIPTION_H
 #define ROBOTDESCRIPTION_H
 
@@ -30,25 +41,37 @@ typedef enum {FORWARD_EULER,
               IMPLICIT_MIDPOINT}
               numerical_integration_scheme;
 
-
+/**
+ * @brief struct to store all user defined input parameters
+ * 
+ */
 struct simParams {
-    double sim_time = 10;                              // Total time for simulation [s]
-    double dt = 1e-3;                                  // Time step size [s]
-    bool render = true;                                // Live OpenGL rendering
-    bool show_mat_frames = false;                      // Render material frames
-    double render_scale = 1.0;                         // Rendering scale
-    double dtol = 1e-2;                                // Dynamics tolerance [m/s]*
-    double ftol = 1e-4;                                // Force tolerance*
-    int max_iter = 500;                                // Maximum iterations for a time step
-    int adaptive_time_stepping = 0;                    // Adaptive time stepping*
-    int cmd_line_per = 1;                              // Command line sim info output period
-    bool enable_2d_sim = false;                        // Lock z and theta DOFs
-    bool line_search = true;                           // Enable line search method
-    numerical_integration_scheme nis = BACKWARD_EULER; // Numerical integration scheme*
-    int debug_verbosity = 1;                           // Prints certain debug statements
+    double sim_time = 10;                              /**< Total time for simulation [s]. */
+    double dt = 1e-3;                                  /**< Time step size [s]. */
+    bool render = true;                                /**< Live OpenGL rendering. */
+    bool show_mat_frames = false;                      /**< Render material frames. */
+    double render_scale = 1.0;                         /**< Rendering scale. */
+    double dtol = 1e-2;                                /**< Dynamics tolerance [m/s]*. */
+    double ftol = 1e-4;                                /**< Force tolerance*. */
+    int max_iter = 500;                                /**< Maximum iterations for a time step. */
+    int adaptive_time_stepping = 0;                    /**< Adaptive time stepping*. */
+    int cmd_line_per = 1;                              /**< Command line sim info output period. */
+    bool enable_2d_sim = false;                        /**< Lock z and theta DOFs. */
+    bool line_search = true;                           /**< Enable line search method. */
+    numerical_integration_scheme nis = BACKWARD_EULER; /**< Numerical integration scheme. */
+    int debug_verbosity = 1;                           /**< Prints certain debug statements. */
 };
 
-
+/**
+ * @brief Get the robot description
+ * 
+ * @param argc 
+ * @param argv 
+ * @param soft_robots the soft robot object
+ * @param forces force container
+ * @param logger logger
+ * @param sim_params simulation input parameters
+ */
 void get_robot_description(int argc, char** argv,
                            const shared_ptr<softRobots>& soft_robots,
                            const shared_ptr<forceContainer>& forces,
