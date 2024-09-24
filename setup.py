3d1e01232080ab8da19c65f14a558a8aa7f79ca5
Andrew Choi <neffneff4@gmail.com>
2024-09-23 23:48:09 -0700

    [Big Feature] Add initial pybind interface (#13)
    Add python versions of helix and spider examples

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, Extension
+import glob
+import os
+
+# Use glob to find the actual .so file with cp310-... in its name
+so_file = glob.glob(os.path.join('py_dismech', 'py_dismech*.so'))[0]  # Adjust path as needed
+
+setup(
+    name='py_dismech',
+    version='0.1',
+    packages=['py_dismech'],
+    package_dir={'py_dismech': 'py_dismech'},
+    package_data={'py_dismech': [os.path.basename(so_file)]},  # Include the .so file
+)
