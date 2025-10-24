f68a1d3127a0c93ecc352463814030baf041efd2
Andrew Choi <neffneff4@gmail.com>
2025-10-24 01:31:56 -0700

    Allow errors to propagate in headless envs

--- src/simulation_environments/headless_sim_env.cpp
+++ src/simulation_environments/headless_sim_env.cpp
@@ -11,16 +11,17 @@ HeadlessSimEnv::HeadlessSimEnv(const std::shared_ptr<World>& m_world,
 HeadlessSimEnv::~HeadlessSimEnv() = default;
 
 void HeadlessSimEnv::stepSimulation() {
-    try {
-        w_p->updateTimeStep();
-    }
-    catch (std::runtime_error& excep) {
-        std::cout << "Caught a std::runtime_error when trying to World->updateTimeStep: "
-                  << excep.what() << std::endl;
-        std::cout << "Attempting clean shutdown..." << std::endl;
-        cleanShutdown();
-        return;
-    }
+    w_p->updateTimeStep();
+    // try {
+    //     w_p->updateTimeStep();
+    // }
+    // catch (std::runtime_error& excep) {
+    //     std::cout << "Caught a std::runtime_error when trying to World->updateTimeStep: "
+    //               << excep.what() << std::endl;
+    //     std::cout << "Attempting clean shutdown..." << std::endl;
+    //     cleanShutdown();
+    //     return;
+    // }
 
     if (is_logging) {
         logger_p->logWorldData();
